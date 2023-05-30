87adc438c828363da2eff0b6118c3a4c4439a4f5
azorophelie <azor.ophelie@yahoo.fr>
2023-05-30 12:25:24 +0200

    azoro

--- SCSS/azor.js
+++ SCSS/azor.js
@@ -1,9 +1,33 @@
-document.querySelectorAll('li').forEach(item => {
-    item.addEventListener('click', darkMode);
-  });
+/*var sidenav = document.getElementById("navbar-clid");
+  var openBtn = document.getElementById("openBtn");
+  var closeBtn = document.getElementById("closeBtn");
   
-  function darkMode() {
-    let body = document.querySelector('body');
-    let mode = this.dataset.mode;
-    body.dataset.theme = mode;
+  openBtn.onclick = openNav;
+  closeBtn.onclick = closeNav;
+  
+  
+  function openNav() {
+    sidenav.classList.add("active");
   }
+  
+  function closeNav() {
+    sidenav.classList.remove("active");
+  }
+  document.addEventListener("keydown", function(event) {
+    if (event.key === "f" || event.key === "F") {
+      var openBtn = document.getElementById("navbar-clcl");
+      navbar-clcl.classList.toggle("show");
+    }
+  }) */
+  document.addEventListener('DOMContentLoaded', function() {
+    var dropDownBtn = document.querySelector('.dropbtn');
+    var dropDownContent = document.querySelector('.dropdown-content');
+  
+    dropDownBtn.addEventListener('click', function() {
+      if (dropDownContent.style.display === 'block') {
+        dropDownContent.style.display = 'none';
+      } else {
+        dropDownContent.style.display = 'block';
+      }
+    });
+  });
--- SCSS/script.js
+++ SCSS/script.js
@@ -18,4 +18,18 @@ window.addEventListener('DOMContentLoaded', function() {
   function toggleDarkMode() {
     var body = document.getElementsByTagName("body")[0];
     body.classList.toggle("dark-mode");
+    darkModeIcon.textContent = body.classList.contains('dark-mode') ? '☀️' : '🌙';
+
   }
+  document.addEventListener('DOMContentLoaded', function() {
+    var dropDownBtn = document.querySelector('.dropbtn');
+    var dropDownContent = document.querySelector('.dropdown-content');
+  
+    dropDownBtn.addEventListener('click', function() {
+      if (dropDownContent.style.display === 'block') {
+        dropDownContent.style.display = 'none';
+      } else {
+        dropDownContent.style.display = 'block';
+      }
+    });
+  });
